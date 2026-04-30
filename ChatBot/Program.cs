5b2bc9dadf327cbd62528a8ae968e082187a738e
idealiveConsulting <idealiveConsulting@gmail.com>
2026-04-30 11:08:07 +0200

    Integración inicial con OpenAI y gestión de .env
    Se añade la dependencia OpenAI, soporte para archivo .env y clase Utilidades para cargar variables de entorno. Se inicializa el cliente de chat en Program.cs usando la clave de API desde .env.

--- ChatBot/Program.cs
+++ ChatBot/Program.cs
@@ -1,1 +1,9 @@
-﻿Console.WriteLine("Hello, World!");
+﻿
+using ChatBot;
+using OpenAI.Chat;
+
+var model = "gpt-5-nano";
+Utilidades.CargarVariableDeEntorno();
+var apiKey = Environment.GetEnvironmentVariable("OPENAI_KEY") 
+                ?? throw new InvalidOperationException("OPENAI_KEY environment variable is not set.");
+var cliente = new ChatClient(model, apiKey);
---
+++ ChatBot/Utilidades.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChatBot;
+
+internal class Utilidades
+{
+
+    internal static void CargarVariableDeEntorno()
+    {
+        foreach (var linea in File.ReadAllLines(".env"))
+        {
+            // LLAVE=VALOR
+            var partes = linea.Split('=', 2);
+            if (partes.Length == 2) {
+                var llave = partes[0].Trim();
+                var valor = partes[1].Trim();
+                Environment.SetEnvironmentVariable(llave, valor);
+            }   
+
+        }
+
+    }
+
+
+}
